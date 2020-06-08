39533770229cb31d1a009accb8e7fef7e439b418
Jameslambdastudent <63371643+Jameslambdastudent@users.noreply.github.com>
2020-06-07 23:23:14 -0400

    finish homework

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+const button = document.getElementsByClassName('.btn')
+
+button.addEventListener('click' , runEvent)
+button.addEventListener('dbclick', runEvent)
+button.addEventListener('mousedown', runEvent)
+button.addEventListener('mouseup', runEvent)
+button.addEventListener('mouseenter', runEvent)
+button.addEventListener('mouseleave', runEvent)
+button.addEventListener('mouseout', runEvent)
+button.addEventListener('mousemove', runEvent)
+button.addEventListener('submit', runEvent)
+
+
+const navBar = document.getElementsByClassName('nav')
+navBar.addEventListener('dbclick', (e) => {
+navBar.style.color = 'green'
+e.preventDefault()
+})
+
+button.addEventListener('click', (e) => {
+       console.log('middle');
+      button.style.color = 'green';
+      e.stopPropagation();
+     });
+
+
+
+
+
+
+function runEvent(e) {
+ console.log(`EVENT TYPE: ${e.type}`)
+}
